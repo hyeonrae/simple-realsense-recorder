4dbe15329d99326c1c3b8c3f7b71b61f34f113ee
hyeonrae <hyeonrae.cho@gmail.com>
2020-09-25 09:59:36 +0900

    add

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+cp -v srr ~/.local/bin/
+cp -v srrcvt ~/.local/bin/
+cp -v srrmkvid ~/.local/bin/
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+[ -f ~/.local/bin/srr ] && rm -v ~/.local/bin/srr
+[ -f ~/.local/bin/srrcvt ] && rm -v ~/.local/bin/srrcvt
+[ -f ~/.local/bin/srrmkvid ] && rm -v ~/.local/bin/srrmkvid
